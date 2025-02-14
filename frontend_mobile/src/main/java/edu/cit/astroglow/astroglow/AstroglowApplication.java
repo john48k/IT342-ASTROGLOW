44b5d6f884cb4ee80e0e96f9b057955bcc07fcdb
LAB14_USER ON WS09 <L14X20W09@citulab.net>
2025-02-14 19:54:10 +0800

    add mobile folder

---
+++ frontend_mobile/src/main/java/edu/cit/astroglow/astroglow/AstroglowApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.astroglow.astroglow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroglowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroglowApplication.class, args);
+	}
+
+}
