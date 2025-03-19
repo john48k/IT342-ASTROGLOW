c5bc299cc1aa3cc7ef730d269bcc2ec02871c57f
Imo-sama <allenluis.mangoroban@cit.edu>
2025-03-19 20:43:58 +0800

    NEW BACKEND

---
+++ backend/AstroGlow/src/test/java/com/AstroGlow/AstroGlow/AstroGlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AstroGlow.AstroGlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroGlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
