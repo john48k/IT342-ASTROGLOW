604049c3a3b2c0ebe80e1db814bf81a3836ce5c4
john48k ⌖ <johncanal300@gmail.com>
2025-03-20 21:51:33 +0800

    about page done 4 now

--- frontend_web/astro/src/Pages/AboutPage/AboutPage.jsx
+++ frontend_web/astro/src/Pages/AboutPage/AboutPage.jsx
@@ -10,10 +10,29 @@ const AboutPage = () => {
                 <h1 className='hero-title'>About Us</h1>
                 <p className='hero-sub-title'>"Where Music Meets the Universe – Get to Know AstroGlow's Creators."</p>
             </div>
-            <div className=''></div>
-            <h1>
-                Placeholder
-            </h1>
+            <div className="cards-section">
+                <div className="card">
+                    <img src="john-pfp.png" alt="Profile 1" className="card-image" />
+                    <div className="card-content">
+                        <h2 className="card-name">John Gabriel Cañal</h2>
+                        <p className="card-course">BSIT-3</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="allen-pfp.jpg" alt="Profile 2" className="card-image" />
+                    <div className="card-content">
+                        <h2 className="card-name">Allen Luis Mangoroban</h2>
+                        <p className="card-course">BSIT-3</p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src="cg-pfp.jpg" alt="Profile 3" className="card-image" />
+                    <div className="card-content">
+                        <h2 className="card-name">Cg Fernandez</h2>
+                        <p className="card-course">BSIT-3</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
