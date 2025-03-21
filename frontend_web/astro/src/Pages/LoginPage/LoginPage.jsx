147d6ebd26ebc5c5b7add116aeb01c03ba075ded
john48k⌖ <johncanal300@gmail.com>
2025-03-21 20:53:31 +0800

    done design login page

--- frontend_web/astro/src/Pages/LoginPage/LoginPage.jsx
+++ frontend_web/astro/src/Pages/LoginPage/LoginPage.jsx
@@ -1,29 +1,40 @@
-import './LoginPage.css'
+import styles from "./LoginPage.module.css";
 
 const LoginPage = () => {
-    return (
-        <div className="LoginPage">
-            <section className="container">
-                <div className="login-container">
-                    <div className="circle circle-one"></div>
-                    <div className="form-container">
-                        <img src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png" alt="illustration" className="illustration" />
-                        <h1 className="opacity">LOGIN</h1>
-                        <form>
-                            <input type="text" placeholder="EMAIL" />
-                            <input type="password" placeholder="PASSWORD" />
-                            <button className="opacity">SUBMIT</button>
-                        </form>
-                        <div className="register-forget opacity">
-                            <a className='register-hover' href="/signup">REGISTER</a>
-                        </div>
-                    </div>
-                    <div className="circle circle-two"></div>
-                </div>
-                <div className="theme-btn-container"></div>
-            </section>
+  return (
+    <div className={styles.LoginPage}>
+      <section className={styles.container}>
+        <div className={styles["login-container"]}>
+          <div className={`${styles.circle} ${styles["circle-one"]}`}></div>
+          <div className={styles["form-container"]}>
+            {/* <img
+              src="https://raw.githubusercontent.com/hicodersofficial/glassmorphism-login-form/master/assets/illustration.png"
+              alt="illustration"
+              className={styles.illustration}
+            /> */}
+            <img
+              src="login-character.png"
+              alt="login-character"
+              className={styles.illustration}
+            />
+            <h1 className={styles.opacity}>LOGIN</h1>
+            <form>
+              <input type="text" placeholder="EMAIL" />
+              <input type="password" placeholder="PASSWORD" />
+              <button className={styles.opacity}>SUBMIT</button>
+            </form>
+            <div className={`${styles["register-forget"]} ${styles.opacity}`}>
+              <a className={styles["register-hover"]} href="/signup">
+                REGISTER
+              </a>
+            </div>
+          </div>
+          <div className={`${styles.circle} ${styles["circle-two"]}`}></div>
         </div>
-    );
+        <div className={styles["theme-btn-container"]}></div>
+      </section>
+    </div>
+  );
 };
 
 export default LoginPage;
