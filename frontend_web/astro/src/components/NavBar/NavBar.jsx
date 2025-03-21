9df045e664b4819bd2d9716400fc14dd5e8f021b
john48k⌖ <johncanal300@gmail.com>
2025-03-21 21:02:03 +0800

    fix navbar navigation of login

--- frontend_web/astro/src/components/NavBar/NavBar.jsx
+++ frontend_web/astro/src/components/NavBar/NavBar.jsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 // import logo from '../assets/images/AstroGlow-logo.png';
-import logo from '../../assets/images/AstroGlow-logo.png'; // Corrected path
+import logo from "../../assets/images/AstroGlow-logo.png"; // Corrected path
 
 const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-logo">
-                <Link to="/home" style={{ display: 'flex', alignItems: 'center' }}>
-                    <img src={logo} alt="AstroGlow Logo" />
-                    <h1 className='navbar-title-text'>AstroGlow</h1>
-                </Link>
-            </div>
-            <div className="navbar-links">
-                <Link to="/about" className='about-us'>About Us</Link>
-                <p className="navbar-separate">|</p>
-                <button className="navbar-button navbar-login-button">Login</button>
-                <Link to="/signup" className="navbar-button navbar-signup-button">Sign Up</Link>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/home" style={{ display: "flex", alignItems: "center" }}>
+          <img src={logo} alt="AstroGlow Logo" />
+          <h1 className="navbar-title-text">AstroGlow</h1>
+        </Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/about" className="about-us">
+          About Us
+        </Link>
+        <p className="navbar-separate">|</p>
+        <Link to="/login" className="navbar-button navbar-login-button">Login</Link>
+        <Link to="/signup" className="navbar-button navbar-signup-button">
+          Sign Up
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
