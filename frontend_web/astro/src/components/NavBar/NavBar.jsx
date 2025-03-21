abeacc12b02e4ec5cb93cc822438181bfcc378bb
john48k⌖ <johncanal300@gmail.com>
2025-03-21 22:01:29 +0800

    redesigned about us + fix navbar login

--- frontend_web/astro/src/components/NavBar/NavBar.jsx
+++ frontend_web/astro/src/components/NavBar/NavBar.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-// import logo from '../assets/images/AstroGlow-logo.png';
-import logo from '../../assets/images/AstroGlow-logo.png'; // Corrected path
+import logo from '../../assets/images/AstroGlow-logo.png';
 
 const Navbar = () => {
     return (
@@ -16,7 +15,8 @@ const Navbar = () => {
             <div className="navbar-links">
                 <Link to="/about" className='about-us'>About Us</Link>
                 <p className="navbar-separate">|</p>
-                <button className="navbar-button navbar-login-button">Login</button>
+                {/* <button className="navbar-button navbar-login-button">Login</button> */}
+                <Link to="/login" className="navbar-button navbar-login-button">Login</Link>
                 <Link to="/signup" className="navbar-button navbar-signup-button">Sign Up</Link>
             </div>
         </nav>
