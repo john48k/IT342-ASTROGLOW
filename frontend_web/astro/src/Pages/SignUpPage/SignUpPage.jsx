81d949270302a5102dc29c1e2f91555db017deb7
john48k ⌖ <johncanal300@gmail.com>
2025-03-21 11:36:11 +0800

    signup page draft ready

--- frontend_web/astro/src/Pages/SignUpPage/SignUpPage.jsx
+++ frontend_web/astro/src/Pages/SignUpPage/SignUpPage.jsx
@@ -1,11 +1,41 @@
-import './SignUpPage.css'
-import NavBar from '../../components/NavBar/NavBar'
+import './SignUpPage.css';
+import '../../components/NavBar/NavBar.css'; // Import NavBar CSS
+import NavBar from '../../components/NavBar/NavBar';
 
 const SignUpPage = () => {
     return (
         <div className="SignUpPage">
             <NavBar />
-            <h1>SignUpPage Component</h1>
+            <div className="split-screen">
+                <div className="left-side">
+                    {/* <img src="path/to/your/image.jpg" alt="Sign Up" className="full-size-image" /> */}
+                </div>
+                <div className="right-side">
+                    <form className="signup-form">
+                        <h2>Sign up to AstroGlow</h2>
+                        <div className="form-group">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id="username" name="username" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" name="email" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="password">Password</label>
+                            <input type="password" id="password" name="password" required />
+                        </div>
+                        <button type="submit" className="signup-button">Sign up</button>
+                        <div className="google-login">
+                            <a href="http://google.com/">Login with Google</a>
+                        </div>
+                        <div className="login-link">
+                            <span>Already have an account? </span>
+                            <a href="/login">Login</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
