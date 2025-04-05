f21a4ed82c23d0334fe71b090173e4815a4ef72c
john48k ⌖ <johncanal300@gmail.com>
2025-04-05 21:37:19 +0800

    update about page roles

--- frontend_web/astro/src/Pages/AboutPage/AboutPage.jsx
+++ frontend_web/astro/src/Pages/AboutPage/AboutPage.jsx
@@ -20,21 +20,27 @@ const AboutPage = () => {
                     <img src="cg-pfp.jpg" alt="Cg Fernandez" className={styles.creatorImage} />
                     <div className={styles.creatorInfo}>
                         <h1 className={styles.name}>Cg Fernandez</h1>
-                        <p className={styles.course}>BSIT-3</p>
+                        {/* <p className={styles.course}>Mobile Frontend Development</p> */}
+                        <p className="text-[22px]">Mobile Frontend Development</p>
+
+
                     </div>
                 </div>
                 <div className={styles.creatorContainer}>
                     <img src="john-pfp.png" alt="John Gabriel Cañal" className={styles.creatorImage} />
                     <div className={styles.creatorInfo}>
                         <h1 className={styles.name}>John Gabriel Cañal</h1>
-                        <p className={styles.course}>BSIT-3</p>
+                        {/* <p className={styles.course}>Web Frontend Development</p> */}
+                        <p className="text-[22px]">Web Frontend Development</p>
+
                     </div>
                 </div>
                 <div className={styles.creatorContainer}>
                     <img src="allen-pfp.jpg" alt="Allen" className={styles.allencreatorImage} />
                     <div className={styles.creatorInfo}>
                         <h1 className={styles.name}>Allen Luis Mangoroban</h1>
-                        <p className={styles.course}>BSIT-3</p>
+                        {/* <p className={styles.course}>Backend Developer</p> */}
+                        <p className="text-[22px]">Backend Developer</p>
                     </div>
                 </div>
             </div>
