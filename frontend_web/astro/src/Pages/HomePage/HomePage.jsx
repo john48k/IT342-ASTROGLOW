db309f48d7d9337de789edb6811c09791f96016d
john48k ⌖ <johncanal300@gmail.com>
2025-04-06 12:43:35 +0800

    tailwind config done holy shittt

--- frontend_web/astro/src/Pages/HomePage/HomePage.jsx
+++ frontend_web/astro/src/Pages/HomePage/HomePage.jsx
@@ -7,6 +7,8 @@ export const HomePage = () => {
     return (
         <div className="flex flex-col">
             <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
+                <h1 className='font-bold text-red-500 text-6xl'>Discover</h1>
+
                 <select />
             </div>
         </div >
