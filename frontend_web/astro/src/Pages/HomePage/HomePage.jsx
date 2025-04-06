d830e7cad399655fdfd00a9975cca1c7a9019204
john48k ⌖ <johncanal300@gmail.com>
2025-04-06 15:53:24 +0800

    what i have so far

--- frontend_web/astro/src/Pages/HomePage/HomePage.jsx
+++ frontend_web/astro/src/Pages/HomePage/HomePage.jsx
@@ -7,13 +7,16 @@ export const HomePage = () => {
     const { data, isFetching, error } = useGetTopChartsQuery();
     const genreTitle = 'Pop';
 
-    console.log(data);
+    if (isFetching) return <Loader title="Loading Songs..." />;
+
+    if (error) return <Error />;
+
     return (
         <div className="flex flex-col bg-gradient-to-br from-black to-purple-800 min-h-screen">
             <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
                 <h2 className='font-bold text-3xl text-white text-left'>Discover {genreTitle}</h2>
                 <select
-                    onchange={() => { }}
+                    onChange={() => { }}
                     value=""
                     className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
                 >
@@ -21,7 +24,7 @@ export const HomePage = () => {
                 </select>
             </div>
             <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
-                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+                {data?.map((song, i) => (
                     <SongCard
                         key={song.key}
                         song={song}
