ee60b7a34cac510ea44142cae471796119ea42ea
john48k ⌖ <johncanal300@gmail.com>
2025-04-06 13:12:59 +0800

    map added to test

--- frontend_web/astro/src/Pages/HomePage/HomePage.jsx
+++ frontend_web/astro/src/Pages/HomePage/HomePage.jsx
@@ -3,13 +3,28 @@ import { genres } from '../../assets/constants';
 import { Link } from "react-router-dom";
 
 export const HomePage = () => {
-    console.log(genres);
+    // console.log(genres);
+    const genreTitle = 'Pop';
     return (
-        // <div className="flex flex-col bg-blue-900 min-h-screen" >
         <div className="flex flex-col bg-gradient-to-br from-black to-purple-800 min-h-screen">
             <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
-
-                <select />
+                <h2 className='font-bold text-3xl text-white text-left'>Discover {genreTitle}</h2>
+                <select
+                    onchange={() => { }}
+                    value=""
+                    className='bg-black text-gray-300 p-3 text-sm rounded-lg outline-none sm:mt-0 mt-5'
+                >
+                    {genres.map((genre) => <option key={genre.value} value={genre.value}>{genre.title}</option>)}
+                </select>
+            </div>
+            <div className='flex flex-wrap sm:justify-start justify-center gap-8'>
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+                    <SongCard
+                        key={song.key}
+                        song={song}
+                        i={i}
+                    />
+                ))}
             </div>
         </div >
     );
