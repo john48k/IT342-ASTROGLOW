fdd5f00cfa7d68ce118e43307c42a0dd767ab9a2
john48k ⌖ <johncanal300@gmail.com>
2025-04-06 13:03:07 +0800

    change the bg to purple to black tailwind

--- frontend_web/astro/src/App.jsx
+++ frontend_web/astro/src/App.jsx
@@ -19,6 +19,7 @@ function App() {
         <Route path="/profile" element={<UserProfilePage />} />
       </Routes>
     </UserProvider>
+
   );
 }
 
--- frontend_web/astro/src/Pages/HomePage/HomePage.jsx
+++ frontend_web/astro/src/Pages/HomePage/HomePage.jsx
@@ -5,9 +5,9 @@ import { Link } from "react-router-dom";
 export const HomePage = () => {
     console.log(genres);
     return (
-        <div className="flex flex-col">
+        // <div className="flex flex-col bg-blue-900 min-h-screen" >
+        <div className="flex flex-col bg-gradient-to-br from-black to-purple-800 min-h-screen">
             <div className='w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10'>
-                <h1 className='font-bold text-red-500 text-6xl'>Discover</h1>
 
                 <select />
             </div>
