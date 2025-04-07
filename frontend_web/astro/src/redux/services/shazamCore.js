ca24324e257e3e013cb81aa7c6b9be00494bd1a5
john48k ⌖ <johncanal300@gmail.com>
2025-04-07 20:47:06 +0800

    Update shazamCore new api fix try try try

--- frontend_web/astro/src/redux/services/shazamCore.js
+++ frontend_web/astro/src/redux/services/shazamCore.js
@@ -3,37 +3,59 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shazam-core.p.rapidapi.com/",
+    baseUrl: "https://shazam-core7.p.rapidapi.com/",
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
         import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY ||
           "5f4e5ac0a3mshc04b79b6d5344a5p1ab1eejsnb9fd583022dd"
       );
-      headers.set("X-RapidAPI-Host", "shazam-core.p.rapidapi.com");
+      headers.set("X-RapidAPI-Host", "shazam-core7.p.rapidapi.com");
       return headers;
     },
   }),
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => "v1/charts/world" }),
+    getTopCharts: builder.query({
+      query: () => ({
+        url: "charts/get-top-songs-in-world",
+        params: { limit: "10" },
+      }),
+    }),
     getSongsByGenre: builder.query({
-      query: (genre) => `v1/charts/genre-world?genre_code=${genre}`,
+      query: (genre) => ({
+        url: "charts/get-top-songs-in-genre-world",
+        params: { genre_code: genre, limit: "10" },
+      }),
     }),
     getSongsByCountry: builder.query({
-      query: (countryCode) => `v1/charts/country?country_code=${countryCode}`,
+      query: (countryCode) => ({
+        url: "charts/get-top-songs-in-country",
+        params: { country_code: countryCode, limit: "10" },
+      }),
     }),
     getSongsBySearch: builder.query({
-      query: (searchTerm) =>
-        `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}`,
+      query: (searchTerm) => ({
+        url: "search",
+        params: { term: searchTerm, limit: "10" },
+      }),
     }),
     getArtistDetails: builder.query({
-      query: (artistId) => `v2/artists/details?artist_id=${artistId}`,
+      query: (artistId) => ({
+        url: "artist/get-details",
+        params: { id: artistId },
+      }),
     }),
     getSongDetails: builder.query({
-      query: ({ songid }) => `v1/tracks/details?track_id=${songid}`,
+      query: ({ songid }) => ({
+        url: "songs/get-details",
+        params: { id: songid },
+      }),
     }),
     getSongRelated: builder.query({
-      query: ({ songid }) => `v1/tracks/related?track_id=${songid}`,
+      query: ({ songid }) => ({
+        url: "songs/get-related-artist",
+        params: { id: songid, limit: "10" },
+      }),
     }),
   }),
 });
