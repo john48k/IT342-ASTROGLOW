5d2536f1a0d1f3f4d56426c05f92b77cc44764b5
Cg Fernandez <fcg8963@gmail.com>
2025-04-22 09:55:58 +0800

    Update UserRepository.java

--- backend/AstroGlow/src/main/java/com/astroglow/Repository/UserRepository.java
+++ backend/AstroGlow/src/main/java/com/astroglow/Repository/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     UserEntity findByUserName(String userName);
     UserEntity findByOauthId(String oauthId);
+    UserEntity findByUserEmail(String userEmail);
 }
